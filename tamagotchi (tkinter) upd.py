a2703c8e2107b701e5c943cc552d41dff7eba117
alonevv <123638768+alonevv@users.noreply.github.com>
2023-05-18 21:47:25 +0500

    Add files via upload

--- tamagotchi (tkinter) upd.py
+++ tamagotchi (tkinter) upd.py
@@ -7,88 +7,87 @@ from PIL import Image, ImageTk
 weight = 10.00
 thirst = 10.00
 happiness = 10.00
-DecreChar = 0.5
+DecreChar = 0.3
 
 window = Tk()
 window.title("тамагочи")
-window.geometry('400x250')
+window.geometry('410x270')
+window.eval('tk::PlaceWindow . center')
 
 
 def start():
+    def die():
+        messagebox.showerror("тамагочи", "питомец умер")
+        quit()
+
+    def check():
+        global weight, thirst, happiness
+        if (weight > 0) and (thirst > 0) and (happiness > 0):
+            pass
+        else:
+            die()
+
+    def update():
+        global weight, thirst, happiness
+        lbl1.config(text=("голод", round(weight, 2)))
+        lbl2.config(text=("жажда", round(thirst, 2)))
+        lbl3.config(text=("счастье", round(happiness, 2)))
+
     def decre():
         global weight, thirst, happiness
         weight -= DecreChar
         thirst -= DecreChar
         happiness -= DecreChar
+
     def food():
-        lbl4.grid_remove()
         global weight, thirst, happiness
+        lbl4.grid_remove()
         decre()
-        if (weight > 0.03) and (thirst > 0.03) and (happiness > 0.03):
-            if (weight < 9.75) and (weight > 0):
-                weight = weight + 0.25
-                thirst = thirst - 0.06
-            else:
-                lbl4.config(text="Не голоден")
-                lbl4.grid(column=0, row=4)
-            lbl1.config(text=("голод", round(weight, 2)))
-            lbl2.config(text=("жажда", round(thirst, 2)))
-            lbl3.config(text=("счастье", round(happiness, 2)))
+        if (weight < 7.5) and (weight > 0):
+            weight = weight + 2.5
+            thirst = thirst - 0.6
         else:
-            messagebox.showerror("тамагочи", "питомец умер")
-            quit()
+            lbl4.config(text="Не голоден")
+            lbl4.grid(column=0, row=4)
+        update()
+        check()
 
     def water():
         lbl4.grid_remove()
         global weight, thirst, happiness
         decre()
-        if (weight > 0.03) and (thirst > 0.03) and (happiness > 0.03):
-            if (thirst < 9.75) and (thirst > 0):
-                thirst = thirst + 0.25
-                weight = weight - 0.07
-            else:
-                lbl4.config(text="Не хочет пить")
-                lbl4.grid(column=0, row=4)
-            lbl1.config(text=("голод", round(weight, 2)))
-            lbl2.config(text=("жажда", round(thirst, 2)))
-            lbl3.config(text=("счастье", round(happiness, 2)))
+        if (thirst < 7.5) and (thirst > 0):
+            thirst = thirst + 2.5
+            weight = weight - 0.6
         else:
-            messagebox.showerror("тамагочи", "питомец умер")
-            quit()
+            lbl4.config(text="Не хочет пить")
+            lbl4.grid(column=0, row=4)
+        update()
+        check()
 
     def play():
         lbl4.grid_remove()
         global weight, thirst, happiness
         decre()
-        if (weight > 0.03) and (thirst > 0.03) and (happiness > 0.03):
-            if (happiness < 9.75) and (happiness > 0):
-                happiness = happiness + 0.25
-                thirst = thirst - 0.07
-                weight = weight - 0.06
-            else:
-                lbl4.config(text="не хочет играть")
-                lbl4.grid(column=0, row=4)
-            lbl1.config(text=("голод", round(weight, 2)))
-            lbl2.config(text=("жажда", round(thirst, 2)))
-            lbl3.config(text=("счастье", round(happiness, 2)))
+        if (happiness < 7.5) and (happiness > 0):
+            happiness = happiness + 2.5
+            thirst = thirst - 0.6
+            weight = weight - 0.6
         else:
-            messagebox.showerror("тамагочи", "питомец умер")
-            quit()
+            lbl4.config(text="не хочет играть")
+            lbl4.grid(column=0, row=4)
+        update()
+        check()
 
-    # Функция ignore отвечает за действие "игнорировать". Все переменные остаются равны сами себе.
     def ignore():
         global weight, thirst, happiness
         lbl4.grid_remove()
-        if (weight > 0.03) and (thirst > 0.03) and(happiness > 0.03):
-            decre()
-            lbl1.config(text=("голод",round(weight,2)))
-            lbl2.config(text=("жажда",round(thirst,2)))
-            lbl3.config(text=("счастье",round(happiness,2)))
-        else:
-            messagebox.showerror("тамагочи", "питомец умер")
-            quit()
+        decre()
+        update()
+        check()
+
     def info():
-        messagebox.showinfo('Инструкция',"""
+        messagebox.showinfo('Инструкция', """
 + Вы несете ответственность за жизнь своего питомца, для этого вы должны его кормить,
 поить и играть с ним.
 
@@ -99,8 +98,8 @@ def start():
 + Помните, что если вы игнорируете своего питомца, все его характеристики будут снижаться каждый ход.
 
 + Если вы опустите любую из характеристик до 0, ваш питомец умрет.""")
-    lbl5=Label(window,text="кот Антон",font=font.Font(weight="bold"))
-    lbl5.grid(column=2,row=0)
+    lbl5 = Label(window, text="кот Антон", font=font.Font(weight="bold"))
+    lbl5.grid(column=2, row=0)
     lbl1 = Label(window, text=("голод", weight))
     lbl1.grid(column=0, row=1)
     lbl2 = Label(window, text=("жажда", thirst))
@@ -124,7 +123,9 @@ def start():
     image = Image.open("cat.png")
     image = image.resize((150, 150), Image.LANCZOS)
     photo = ImageTk.PhotoImage(image)
-    image = canvas.create_image(0, 0, anchor='nw', image=photo)
+    canvas.create_image(0, 0, anchor='nw', image=photo)
     canvas.grid(column=2, row=5)
     window.mainloop()
+
+
 start()
